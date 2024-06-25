e23f7612a40543b690bbeaafcf564d1cb5951e63
santi.c <santi.chpy@gmail.com>
2024-06-25 18:12:24 +0700

    update

--- app/Http/Controllers/CustomAuthController.php
+++ app/Http/Controllers/CustomAuthController.php
@@ -17,7 +17,7 @@ class CustomAuthController extends Controller
     public function login()
     {
       //return view('auth.login');
-    return redirect('https://vbnext.vbeyond.co.th');
+    return redirect('https://vbnext.vbeyond.co.th/main');
         //return redirect('http://127.0.0.1:8000/main');
     }
     public function loginUser(Request $request)
